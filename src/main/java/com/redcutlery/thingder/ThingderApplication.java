fc922d5883a0989280fe4e3a3c0360e357940f2c
tk_mini <otm0937@naver.com>
2022-09-06 16:03:35 +0900

    initialize

---
+++ src/main/java/com/redcutlery/thingder/ThingderApplication.java
@@ -0,0 +1,13 @@
+package com.redcutlery.thingder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThingderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redcutlery/thingder/ThingderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redcutlery.thingder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThingderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
